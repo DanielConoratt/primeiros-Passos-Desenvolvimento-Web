b45463122c195634f75433a66c2c617c09161043
DanielConoratt <conoratt@gmail.com>
2022-06-24 19:08:18 -0300

    Atualização módulo 2

---
+++ index1.php
@@ -0,0 +1,29 @@
+<htmal>
+
+	<head>
+		<title>Meu primeiro site em PHP! Woohoo!</title>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+		<style type="text/css">
+			.linha {
+				font-weight: bold;
+				color: green;
+				padding-left: 15px;
+				line-height: 40px;
+			}
+		</style>	
+	</head>
+	
+	<body>
+		<?php
+			for ($i = 0 ; $i < 10 ; $i++ ){
+				print( "<span class=\"linha\">Linha número " . $i . "</span><br />");
+			}
+		?>
+	</body>
+	
+	<script type="text/javascript">
+		$(document).ready(function(){
+			alert("Woohoo!");
+		});
+	</script>	
+</html>	
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Primeiros passos no desenvolvimento web em PHP
+
+# Contem um desafio utilizando o jquery accordion
+
+ 
+
